d93fab2717f7b72d46c75536d88e7b518e67bff6
fuselayer <145614503+fuselayer@users.noreply.github.com>
2025-10-31 02:36:33 -0400

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# Minimal Workflow Image Extension
+# This is a minimal ComfyUI custom node that provides only the workflow image functionality
+
+# Since this is a web-only extension, no Python nodes are defined
+# All functionality is implemented in web/js/workflowImage.js
+
+WEB_DIRECTORY = "./web"
+
+__all__ = []
